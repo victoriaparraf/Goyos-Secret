ff5a9e4f812bf70cbf3b00ac42c9570147ded45e
Dragoner91 <Dragoner919@hotmail.com>
2025-05-27 22:41:27 -0400

    feat: :tada: initialize FastAPI and test endpoint

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Goyo´s Secrets Restaurants API",
+    description="API para gestionar restaurantes, reservaciones, usuarios y menus.",
+    version="1.0.0",
+)
+
+@app.get("/test", tags=["API Test"])
+async def test_db():
+    try:
+        return {"message": "API corriendo correctamente"}
+    except Exception as e:
+        return {"error": str(e)}
