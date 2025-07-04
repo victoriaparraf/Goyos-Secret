acc27bb9e042d554d934f4addfa17ee4bc5af423
Victoria Vanessa Parra Franco <victoriaparrafranco@gmail.com>
2025-07-03 21:54:31 -0400

    feat: refactor Restaurant and Table classes to use Pydantic models and add method docstrings in the table repository interface

--- modules/restaurant/domain/restaurant.py
+++ modules/restaurant/domain/restaurant.py
@@ -1,17 +1,10 @@
-from uuid import UUID, uuid4
+from pydantic import BaseModel
+from uuid import UUID
 from datetime import time
 
-class Restaurant:
-    def __init__(self, id: UUID, name: str, location: str, opening_time: time, closing_time: time):
-        if opening_time >= closing_time:
-            raise ValueError("La hora de apertura debe ser anterior a la hora de cierre.")
-        
-        self.id = id
-        self.name = name
-        self.location = location
-        self.opening_time = opening_time
-        self.closing_time = closing_time
-
-    @staticmethod
-    def create(name: str, location: str, opening_time: time, closing_time: time) -> "Restaurant":
-        return Restaurant(uuid4(), name, location, opening_time, closing_time)
+class Restaurant(BaseModel):
+    id: UUID
+    name: str
+    address: str
+    opening_time: time
+    closing_time: time
--- modules/restaurant/domain/table_repository_interface.py
+++ modules/restaurant/domain/table_repository_interface.py
@@ -6,24 +6,30 @@ from modules.restaurant.domain.table import Table
 class ITableRepository(ABC):
     @abstractmethod
     def get_by_id(self, table_id: UUID) -> Optional[Table]:
+        """Obtener una mesa por su ID."""
         pass
 
     @abstractmethod
     def get_by_restaurant_id(self, restaurant_id: UUID) -> List[Table]:
+        """Obtener todas las mesas de un restaurante por su ID."""
         pass
 
     @abstractmethod
     def get_by_restaurant_and_table_number(self, restaurant_id: UUID, table_number: int) -> Optional[Table]:
+        """Obtener una mesa por el ID del restaurante y el número de mesa."""
         pass
 
     @abstractmethod
     def save(self, table: Table) -> None:
+        """Guardar una mesa en la base de datos."""
         pass
 
     @abstractmethod
     def delete(self, table_id: UUID) -> None:
+        """Eliminar una mesa por su ID."""
         pass
 
     @abstractmethod
     def get_available_tables(self, restaurant_id: UUID, capacity: int, location: str) -> List[Table]:
+        """Obtener mesas disponibles en un restaurante según la capacidad y ubicación."""
         pass
